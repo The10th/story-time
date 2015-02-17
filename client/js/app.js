5c327822c78b309bf8109738d30729bc93e2c91c
Julian Nguyen <julian.hv.nguyen@gmail.com>
2015-02-17 15:12:57 -0800

    service for storyController added

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,6 @@
 //routeStyles is for individual css pages in each partial
 //ngMessages is for form validation
 var app = angular.module('myApp', ['ngRoute','routeStyles', 'ngMessages']);
-
 app.config(function($routeProvider){
 
   $routeProvider
@@ -11,40 +10,59 @@ app.config(function($routeProvider){
 
     templateUrl: 'partials/index.html',
     controller:  'storyController',
-    css: 'css/main.css',
-    reloadOnPath:false
+    css: 'css/main.css'
   }).when('/story', {
 
     templateUrl: 'partials/story.html',
     controller: 'storyController',
-    css: 'css/story.css',
-    reloadOnPath:false
+    css: 'css/story.css'
 
   }).when('/story/:id', {
 
     templateUrl: 'partials/story-path.html',
     controller: 'storyController',
-    css: 'css/story.css',
-    reloadOnPath:false
+    css: 'css/story.css'
 
   //route to root index
   }).when('/story/:id/edit', {
 
     templateUrl: 'partials/story-edit.html',
     controller: 'storyController',
-    css: 'css/story.css',
-    reloadOnPath:false
+    css: 'css/story.css'
 
   //route to root index
   }).when('/not-cool', {
 
     templateUrl: 'partials/not-cool.html',
     controller:  'storyController',
-    css: 'css/main.css',
-    reloadOnPath:false
+    css: 'css/main.css'
 
   }).otherwise({
     redirectTo: '/',
   });
 
+});
+
+app.service('storyService', function(){
+  var storyGroup = [
+
+    //begin story
+    {id: 0, path:'Starting Path', title:'Start', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:1, path:'Path1', title:'Title 1', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:2, path:'Path2', title:'Title 2', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:3, path:'Path3', title:'Title 3', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:4, path:'Path4', title:'Title 4', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:5, path:'Path5', title:'Title 5', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:6, path:'Path6', title:'Title 6', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
+
+    {id:7, path:'Path7', title:'Title 7', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2}
+    ];
+
+    return storyGroup;
 });
--- client/js/controllers/StoryController.js
+++ client/js/controllers/StoryController.js
@@ -1,27 +1,9 @@
 'use strict'
 
-app.controller('storyController', function($scope, $location, $routeParams) {
+app.controller('storyController', function($scope, $location, $routeParams, storyService) {
 	
 	//hardcoded data for now
-	$scope.storyGroup = [
-
-		//begin story
-		{id: 0, path:'Starting Path', title:'Start', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:1, path:'Path1', title:'Title 1', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:2, path:'Path2', title:'Title 2', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:3, path:'Path3', title:'Title 3', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:4, path:'Path4', title:'Title 4', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:5, path:'Path5', title:'Title 5', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:6, path:'Path6', title:'Title 6', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2},
-
-		{id:7, path:'Path7', title:'Title 7', content: 'Migas Shoreditch flannel, asymmetrical vegan skateboard kitsch. Retro fashion axe Echo Park twee put a bird on it vinyl. Helvetica Wes  Anderson ugh Truffaut leggings single-origin coffee. Small batch cronut Williamsburg, wolf cardigan sriracha 90', choice1:1, choice2: 2}
-	];
+	$scope.storyGroup = storyService;
 
 	if($routeParams.id){
 		$scope.story = $scope.storyGroup[$routeParams.id];
